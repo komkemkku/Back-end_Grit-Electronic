ed65195ceec5f848334dfbbbd5f77eda428686c1
komkem <komkem.k@kkumail.com>
2025-01-06 17:11:02 +0700

    update

--- model/payments.model.go
+++ model/payments.model.go
@@ -5,14 +5,14 @@ import "github.com/uptrace/bun"
 type Payments struct {
 	bun.BaseModel `bun:"table:payments"`
 
-	ID     int   `bun:",type:serial,autoincrement,pk"`
-	Price  float64 `bun:"price"`
-	Amount int   `bun:"amount"`
-	Slip   string  `bun:"slip"`
-	Status int  `bun:"status"`
-	BankName string `bun:"bank_name"`
-	AccountName string `bun:"account_name"`
-	AccountNumber string `bun:"account_number"`
+	ID            int     `bun:",type:serial,autoincrement,pk"`
+	Price         float64 `bun:"price"`
+	Amount        int     `bun:"amount"`
+	Slip          string  `bun:"slip"`
+	Status        int     `bun:"status"`
+	BankName      string  `bun:"bank_name"`
+	AccountName   string  `bun:"account_name"`
+	AccountNumber string  `bun:"account_number"`
 
 	CreateUnixTimestamp
 	UpdateUnixTimestamp
