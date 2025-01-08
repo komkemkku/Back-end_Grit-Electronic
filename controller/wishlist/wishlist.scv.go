dd91e0a936f4b26b6e2cdf2078461099f4ed62fe
naza007 <std.64122970111@ubru.ac.th>
2025-01-08 17:13:20 +0700

    wi

--- controller/wishlist/wishlist.scv.go
+++ controller/wishlist/wishlist.scv.go
@@ -24,13 +24,13 @@ func ListWishlistsService(ctx context.Context, req requests.WishlistsRequest) ([
 	// สร้าง query
 	query := db.NewSelect().
 		TableExpr("wishlists AS w").
-		Column("p.id", "p.created_at", "p.updated_at").
-		ColumnExpr("p.id AS product__id").
-		ColumnExpr("p.name AS product__name").
-		Join("LEFT JOIN products AS p ON p.id = w.product_id")
+		Column("w.id", "w.created_at", "w.updated_at").
+		ColumnExpr("w.id AS product__id").
+		ColumnExpr("w.name AS product__name").
+		Join("LEFT JOIN products AS w ON w.id = w.product_id")
 
 	// if req.Search != "" {
-	// 	query.Where("p.name ILIKE ?", "%"+req.Search+"%")
+	//   query.Where("p.name ILIKE ?", "%"+req.Search+"%")
 	// }
 
 	total, err := query.Count(ctx)
@@ -59,10 +59,10 @@ func GetByIdWishlistsService(ctx context.Context, id int64) (*response.WishlistR
 	wish := &response.WishlistResponses{}
 
 	err = db.NewSelect().TableExpr("wishlists AS w").
-	Column("w.id", "p.created_at", "p.updated_at").
-	ColumnExpr("p.id AS product__id").
-	ColumnExpr("p.name AS product__name").
-	Join("LEFT JOIN products AS p ON p.id = w.product_id").Where("w.id = ?", id).Scan(ctx, wish)
+		Column("w.id", "w.created_at", "w.updated_at").
+		ColumnExpr("w.id AS product__id").
+		ColumnExpr("w.name AS product__name").
+		Join("LEFT JOIN products AS w ON w.id = w.product_id").Where("w.id = ?", id).Scan(ctx, wish)
 	if err != nil {
 		return nil, err
 	}
@@ -79,7 +79,7 @@ func CreateWishlistsService(ctx context.Context, req requests.WishlistsAddReques
 	Wishlists.SetCreatedNow()
 	Wishlists.SetUpdateNow()
 
-	_, err = db.NewInsert().Model(Wishlists).Exec(ctx)
+	_, err := db.NewInsert().Model(Wishlists).Exec(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -87,4 +87,3 @@ func CreateWishlistsService(ctx context.Context, req requests.WishlistsAddReques
 	return Wishlists, nil
 
 }
-
