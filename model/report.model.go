e89e7fa259e4622a7e90966e7ee78deb8e24f801
komkem <komkem.k@kkumail.com>
2025-02-18 13:00:30 +0700

    edit model

--- model/report.model.go
+++ model/report.model.go
@@ -1,18 +1,1 @@
 package model
-
-import "github.com/uptrace/bun"
-
-type Report struct {
-	bun.BaseModel `bun:"table:report"`
-
-	ID          int     `bun:",type:serial,autoincrement,pk"`
-	ProductID   int     `bun:"product_id"`
-	ProductName string  `bun:"product_name"`
-	Date        int64   `bun:"date"`
-	TotalPrice  float64 `bun:"total_price"`
-	TotalAmount int     `bun:"total_amount"`
-	Status      string  `bun:"status"`
-
-	CreateUnixTimestamp
-	UpdateUnixTimestamp
-}
