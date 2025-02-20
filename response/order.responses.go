bd60908d38e1bbbec5805ed650117756b79f0d8c
komkem <komkem.k@kkumail.com>
2025-02-20 09:36:54 +0700

    .

--- response/order.responses.go
+++ response/order.responses.go
@@ -23,30 +23,6 @@ type OrderResponses struct {
 	UpdatedAt           int64   `json:"updated_at"`
 }
 
-// type OrderResponses struct {
-// 	ID                  int       `json:"id"`
-// 	UserID              int       `json:"user_id"`
-// 	Username            string    `json:"username"`
-// 	UserFirstname       string    `json:"firstname"`
-// 	UserLastname        string    `json:"lastname"`
-// 	UserPhone           string    `json:"phone"`
-// 	PaymentID           int       `json:"payment_id"`
-// 	ShipmentID          int       `json:"shipment_id"`
-// 	ShipmentFirstname   string    `json:"shipment_firstname"`
-// 	ShipmentLastname    string    `json:"shipment_lastname"`
-// 	ShipmentAddress     string    `json:"shipment_address"`
-// 	ShipmentZipCode     string    `json:"shipment_zip_code"`
-// 	ShipmentSubDistrict string    `json:"shipment_sub_district"`
-// 	ShipmentDistrict    string    `json:"shipment_district"`
-// 	ShipmentProvince    string    `json:"shipment_province"`
-// 	TotalPrice          float64   `json:"total_price"`
-// 	TotalAmount         int       `json:"total_amount"`
-// 	TrackingNumber      string    `json:"tracking_number"`
-// 	Status              string    `json:"status"`
-// 	CreatedAt           time.Time `json:"created_at"`
-// 	UpdatedAt           time.Time `json:"updated_at"`
-// }
-
 type OrderRespOrderDetail struct {
 	ID             int                     `json:"id"`
 	User           UserRespOrderDetail     `bun:"user"`
@@ -70,21 +46,6 @@ type ProductInfo struct {
 	Image              string  `json:"image"`
 	IsReview           bool    `json:"is_review"`
 }
-
-// type OrderRespOrderDetail struct {
-// 	ID             int                     `json:"id"`
-// 	User           UserRespOrderDetail     `bun:"user"`
-// 	Products       []string                `json:"product"`
-// 	Payment        PaymentRespOrderDetail  `bun:"payment"`
-// 	SystemBank     SystemBankRespPayment   `bun:"system_bank"`
-// 	Shipment       ShipmentRespOrderDetail `bun:"shipment"`
-// 	TotalAmount    int                     `json:"total_amount"`
-// 	TotalPrice     float64                 `json:"total_price"`
-// 	TrackingNumber string                  `json:"tracking_number"`
-// 	Status         string                  `json:"status"`
-// 	Created_at     int64                   `json:"created_at"`
-// 	Updated_at     int64                   `json:"updated_at"`
-// }
 
 type OrderRespReport struct {
 	ID         int   `json:"id"`
